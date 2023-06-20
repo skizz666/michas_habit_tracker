b2822e1ba3d72aba2d7cd07acc28fef667287841
skizz666 <101053691+skizz666@users.noreply.github.com>
2023-06-20 13:51:32 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+habits = ["Test habit"]
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", habits=habits, title="Habit Tracker - Home")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_habit():
+    if request.form == "POST":
+        habit = request.form.get("habit")
+        habits.append(habit)
+    return render_template("add_habit.html", title="Habit Tracker- Add Habit")
